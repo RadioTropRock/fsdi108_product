5b54d7bce7655b29fabb065351a17f77bb2cbc58
Eric Babin <eric@babin.com>
2021-03-20 13:09:48 -0500

    second commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react';
+import './footer.css';
+
+class Footer extends Component {
+    render() { 
+        return (
+            <React.Fragment>
+            <div className="footer-cr">
+                2021 All Rights Reserved. 
+            </div>
+            <label className="footer-name">Eric Babin</label>
+            </React.Fragment>
+          );
+    }
+}
+ 
+export default Footer;
---
+++ src/components/product.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import QuantityPicker from "./quantityPicker";
+import "./product.css";
+
+class Product extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="product">
+        <img src= "https://picsum.photos/200/300"></img>
+
+        <label className="description">Product Description here</label>
+
+        <label>$ Total</label>
+        <label>$ Price</label>
+
+        <QuantityPicker></QuantityPicker>
+      </div>
+    );
+  }
+}
+
+export default Product;
